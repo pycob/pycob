f74650d2f4604ef7ed6e69272bf71ed4c65738e0
Zain Hoda <7146154+zainhoda@users.noreply.github.com>
2023-01-17 19:21:39 -0500

    query param

--- src/pycob/request.py
+++ src/pycob/request.py
@@ -3,3 +3,7 @@ import flask
 class Request:
     def __init__(self, flask_request: flask.Request):
         self.flask_request = flask_request
+    
+    def get_query_parameter(self, key: str) -> str:
+        unsanitized = self.flask_request.args.get(key, '')
+        return unsanitized.replace('<', '&lt;').replace('>', '&gt;').replace("'", "''").replace(";", "")
