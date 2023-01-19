6399c5b5a587017ecd7fc98245cc200e9ca0b1f1
Zain Hoda <7146154+zainhoda@users.noreply.github.com>
2023-01-18 22:07:44 -0500

    pandas table

--- src/pycob/page.py
+++ src/pycob/page.py
@@ -20,17 +20,61 @@ class Page:
     self.components.append(component)
     return self
 
+  def __add_pandastable(self, df):
+    # Pandas dataframe to html
+    html = '''<div class="p-8">'''
+
+    html += '''<div class="relative overflow-x-auto shadow-md sm:rounded-lg">'''
+
+    html += '''<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">'''
+
+    # Pandas DataFrame columns to 
+    html += '''<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">'''
+
+    html += "<tr>"
+
+    # Get df index name
+    if df.index.name is not None:
+      html += '''<th scope="col" class="px-6 py-3">''' + df.index.name +  "</th>"
+
+    for column in df.columns:
+        html += '''<th scope="col" class="px-6 py-3">''' + column + "</th>"
+
+    html += "</tr>"
+
+    html += "</thead>"
+
+    # Pandas DataFrame rows to html
+    html += "<tbody>"
+    i = 0
+    for index, row in df.iterrows():
+        if i % 2 == 0:
+          html += '''<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">'''
+        else:
+          html += '''<tr class="bg-gray-50 border-b dark:bg-gray-900 dark:border-gray-700">'''
+
+        for column in df.columns:
+            html += '''<td class="px-6 py-4">''' + str(row[column]) + "</td>"
+        html += "</tr>"
+        i += 1
+
+    html += "</tbody>"
+
+    html += "</table>"
+
+    html += "</div>"
+
+    html += "</div>"
+
+    self.components.append(HtmlComponent(html))
+    return self
+
   ## Add to code generation
 
   def add_component(self, component: Component):
     self.components.append(component)
     return self
 
-  def add_pandas_table(self, df):
-    # TODO: Move the logic out to a separate function that operates on components
-    # https://stackoverflow.com/a/48363085/3179416
-    self.components.append(HtmlComponent(df.to_html()))
-    return self
 
 ## Begin Generated Code
 
@@ -73,6 +117,9 @@ class Page:
     self.components.append(SectionComponent(id, name, level, icon))
     return self
 
+  def add_pandastable(self, dataframe: str):
+    self.__add_pandastable(dataframe)
+    return self
 
 _tailwind_header_to_sidebar = '''
 <!doctype html>
