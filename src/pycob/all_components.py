d39190468ac4cb5aefb3c6127f0f790123d5794f
Zain Hoda <7146154+zainhoda@users.noreply.github.com>
2023-01-30 14:21:19 -0500

    remove localStorage for now

--- src/pycob/all_components.py
+++ src/pycob/all_components.py
@@ -221,11 +221,11 @@ class CodeeditorComponent(Component):
     editor.setTheme("ace/theme/monokai");
     editor.session.setMode("ace/mode/''' + self.language + '''");
 
-    const savedCode = localStorage.getItem('code');
+    // const savedCode = localStorage.getItem('code');
 
-    if (savedCode) {
-        editor.setValue(savedCode);
-    }
+    // if (savedCode) {
+    //     editor.setValue(savedCode);
+    // }
 </script>'''
 
 class ContainerComponent(Component):
@@ -768,8 +768,6 @@ class ListitemComponent(Component):
             self.svg = '''<svg class="w-4 h-4 mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>'''
         else:
             self.svg = '''<svg class="w-4 h-4 mr-1.5 text-gray-400 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path></svg>'''
-    else:
-        self.svg = ""
 
   def __enter__(self):
     return self
@@ -778,7 +776,7 @@ class ListitemComponent(Component):
     pass
 
   def to_html(self):
-    return '''<li class="flex items-center">
+    return '''<li class="flex items-center ''' + self.classes + '''">
     ''' + self.svg + '''
     ''' + self.value + '''
 </li>'''
