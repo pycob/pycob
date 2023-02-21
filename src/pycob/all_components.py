81f38d81182345fdb4dc6b1c5cee45e698c02089
Zain Hoda <7146154+zainhoda@users.noreply.github.com>
2023-02-21 12:31:31 -0500

    select label

--- src/pycob/all_components.py
+++ src/pycob/all_components.py
@@ -623,7 +623,7 @@ class FormselectComponent(Component):
 
   def to_html(self):
     return '''<div class="mb-6">
-    <label for="''' + self.name + '''" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+    <label for="''' + self.name + '''" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">''' + self.label + '''</label>
     <select id="''' + self.name + '''" name="''' + self.name + '''" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         ''' + '\n'.join(map(lambda x: x.to_html(), self.components)) + ''' 
     </select>
